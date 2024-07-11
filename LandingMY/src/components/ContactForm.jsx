b976a8d97ab48f690120e09ca69ec2420f9bb62b
mlobeto <mercedeslobeto@gmail.com>
2024-07-11 19:46:00 -0300

    ruta vercel

--- LandingMY/src/components/ContactForm.jsx
+++ LandingMY/src/components/ContactForm.jsx
@@ -63,7 +63,7 @@ const ContactForm = forwardRef(({ quoteData, onBack }, ref) => {
     formValues.description = description;
 
     try {
-      const response = await fetch('https://innoweb.azurewebsites.net/emails/send', {
+      const response = await fetch('https://landing-back-five.vercel.app/emails/send', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

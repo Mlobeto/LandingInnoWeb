be3dfbb05a5da865c998b8444d77318f0f16905d
mlobeto <mercedeslobeto@gmail.com>
2024-07-11 21:14:36 -0300

    para onrender

--- BackLanding/app.js
+++ BackLanding/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const userRoutes = require('./src/routes/userRoutes');
 const emailRoutes = require('./src/routes/emailRoutes');
 
 dotenv.config();
@@ -12,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/users', userRoutes);
 app.use('/emails', emailRoutes);
 
 module.exports = app;

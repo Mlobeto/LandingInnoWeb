d4a53d1dc96069a5be87ae2b799727e8074abdfe
yanirc1981 <cerzurc@hotmail.com>
2025-08-26 19:38:07 -0300

    whatsapp

--- LandingMY/src/components/WhatsappButton.jsx
+++ LandingMY/src/components/WhatsappButton.jsx
@@ -5,11 +5,12 @@ const WhatsAppButton = () => {
   return (
     <a
       href="https://wa.me/5492355517802" // Reemplaza con tu número de WhatsApp
-      className="fixed  right-4 bg-azul text-white rounded-full p-3 shadow-lg hover:bg-azul transition duration-300 transform hover:scale-110 hover:animate-bounce z-50 md:bottom-8 md:right-8"
+  className="fixed bottom-20 right-4 md:bottom-8 md:right-8 bg-azul text-white rounded-full p-3 md:p-4 shadow-lg hover:bg-azul transition duration-300 transform hover:scale-110 hover:animate-bounce z-50 flex items-center justify-center"
+      style={{ minWidth: '48px', minHeight: '48px' }}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <FontAwesomeIcon icon={faWhatsapp} size="3x" />
+      <FontAwesomeIcon icon={faWhatsapp} className="w-7 h-7 md:w-10 md:h-10" />
     </a>
   );
 }
